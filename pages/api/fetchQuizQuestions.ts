8eac08e8387693cc664c985b61789c66dab96867
felipeantoniob <73531907+felipeantoniob@users.noreply.github.com>
2022-02-20 12:39:37 +0800

    Fix types

--- pages/api/fetchQuizQuestions.ts
+++ pages/api/fetchQuizQuestions.ts
@@ -1,17 +1,16 @@
 import { shuffleArray } from '../../utils/shuffleArray'
-import { Category, Question, QuestionState } from '../../interfaces/index'
+import { Category, Question, QuestionState, Difficulty } from '../../interfaces/index'
 
 export const fetchQuizQuestions = async (
   numberOfQuestions: number,
-  difficulty: string,
-  category: Category
+  difficulty: Difficulty | undefined,
+  category: Category | undefined
 ): Promise<QuestionState[]> => {
   try {
     const endpoint = `https://opentdb.com/api.php?type=multiple&amount=${numberOfQuestions}${
       difficulty ? `&difficulty=${difficulty}` : ''
     }${category ? `&category=${category.id}` : ''}`
 
-    // console.log(endpoint)
     const response = await fetch(endpoint)
     const data = await response.json()
 
@@ -19,7 +18,8 @@ export const fetchQuizQuestions = async (
       ...question,
       answers: shuffleArray([...question.incorrect_answers, question.correct_answer]),
     }))
-  } catch (err) {
-    console.log(err)
+  } catch (error) {
+    console.error(error)
+    throw error
   }
 }
